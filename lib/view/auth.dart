603898c61e223e1309d611c1ec17184871f271a6
claustra01 <shinya430.g@gmail.com>
2023-09-17 11:20:39 +0900

    :zap: login error dialog

--- lib/view/auth.dart
+++ lib/view/auth.dart
@@ -15,16 +15,37 @@ class AuthPage extends ConsumerStatefulWidget {
 class AuthPageState extends ConsumerState<AuthPage> {
 
   void _loginWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-    final credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth?.accessToken,
-      idToken: googleAuth?.idToken,
-    );
-    User? user =  (await FirebaseAuth.instance.signInWithCredential(credential)).user;
-    if (user != null) {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+      final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+      User? user =  (await FirebaseAuth.instance.signInWithCredential(credential)).user;
+      if (user != null) {
+        // route to home
+        if (!mounted) return;
+        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
+      }
+    } catch (e) {
+      // view error dialog
       if (!mounted) return;
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
+      showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("Error"),
+            content: const Text("Login failed"),
+            actions: [
+              TextButton(
+                child: const Text("OK"),
+                onPressed: () => Navigator.pop(context),
+              ),
+            ],
+          );
+        },
+      );
     }
   }
 
