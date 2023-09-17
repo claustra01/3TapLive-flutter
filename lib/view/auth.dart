7397ab3a4c23c0cfcedaff05b8690b83467bbd23
claustra01 <shinya430.g@gmail.com>
2023-09-17 09:06:05 +0900

    :sparkles: google login

--- lib/view/auth.dart
+++ lib/view/auth.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'package:hackz_tyranno/view/home.dart';
 
 class AuthPage extends ConsumerStatefulWidget {
   const AuthPage({Key? key}) : super(key: key);
@@ -10,8 +14,18 @@ class AuthPage extends ConsumerStatefulWidget {
 
 class AuthPageState extends ConsumerState<AuthPage> {
 
-  void _googleFirebaseAuth() {
-
+  void _loginWithGoogle() async {
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+    User? user =  (await FirebaseAuth.instance.signInWithCredential(credential)).user;
+    if (user != null) {
+      if (!mounted) return;
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomePage()));
+    }
   }
 
   @override
@@ -22,7 +36,7 @@ class AuthPageState extends ConsumerState<AuthPage> {
       ),
       body: Center(
         child: ElevatedButton(
-          onPressed: _googleFirebaseAuth,
+          onPressed: _loginWithGoogle,
           child: const Text('Login with Google')
         ),
       ),
