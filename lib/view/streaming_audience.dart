4c7199c411cce096b6aa65c81ed3b6735fcf7b65
claustra01 <shinya430.g@gmail.com>
2023-09-19 21:29:47 +0900

    :sparkles: streaming audience page

--- lib/view/streaming_audience.dart
+++ lib/view/streaming_audience.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
+
+import 'package:hackz_tyranno/component/video_panel.dart';
 
 class StreamingAudiencePage extends ConsumerStatefulWidget {
   final String channelName;
@@ -12,9 +16,160 @@ class StreamingAudiencePage extends ConsumerStatefulWidget {
 
 class StreamingAudiencePageState extends ConsumerState<StreamingAudiencePage> {
 
+  String appId = dotenv.get('AGORA_APP_ID');
+
+  int uid = 0;
+
+  int? _remoteUid;
+  bool _isJoined = false;
+  final bool _isHost = false;
+  late RtcEngine agoraEngine;
+
+  final GlobalKey<ScaffoldMessengerState> scaffoldMessengerKey
+  = GlobalKey<ScaffoldMessengerState>(); // Global key to access the scaffold
+
+  showMessage(String message) {
+    scaffoldMessengerKey.currentState?.showSnackBar(SnackBar(
+      content: Text(message),
+    ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupVideoSDKEngine();
+
+  }
+
+  @override
+  void dispose() async {
+    await agoraEngine.leaveChannel();
+    agoraEngine.release();
+    super.dispose();
+  }
+
+  Future<void> setupVideoSDKEngine() async {
+    //create an instance of the Agora engine
+    agoraEngine = createAgoraRtcEngine();
+    await agoraEngine.initialize(RtcEngineContext(
+        appId: appId
+    ));
+
+    await agoraEngine.enableVideo();
+
+    // Register the event handler
+    agoraEngine.registerEventHandler(
+      RtcEngineEventHandler(
+        onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+          showMessage("Local user uid:${connection.localUid} joined the channel");
+          setState(() {
+            _isJoined = true;
+          });
+        },
+        onUserJoined: (RtcConnection connection, int remoteUid, int elapsed) {
+          showMessage("Remote user uid:$remoteUid joined the channel");
+          setState(() {
+            _remoteUid = remoteUid;
+          });
+        },
+        onUserOffline: (RtcConnection connection, int remoteUid,
+            UserOfflineReasonType reason) {
+          showMessage("Remote user uid:$remoteUid left the channel");
+          setState(() {
+            _remoteUid = null;
+          });
+        },
+      ),
+    );
+  }
+
+  void join() async {
+
+    // Set channel options
+    ChannelMediaOptions options;
+
+    // Set channel profile and client role
+    if (_isHost) {
+      options = const ChannelMediaOptions(
+        clientRoleType: ClientRoleType.clientRoleBroadcaster,
+        channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+      );
+      await agoraEngine.startPreview();
+    } else {
+      options = const ChannelMediaOptions(
+        clientRoleType: ClientRoleType.clientRoleAudience,
+        channelProfile: ChannelProfileType.channelProfileLiveBroadcasting,
+      );
+    }
+
+    await agoraEngine.joinChannel(
+      token: widget.token,
+      channelId: widget.channelName,
+      options: options,
+      uid: uid,
+    );
+  }
+
+  void leave() {
+    setState(() {
+      _isJoined = false;
+      _remoteUid = null;
+    });
+    agoraEngine.leaveChannel();
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Text(widget.channelName);
+    return MaterialApp(
+      scaffoldMessengerKey: scaffoldMessengerKey,
+      home: Scaffold(
+          body: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 3, vertical: 4),
+            children: [
+              if (_isJoined)
+                Container(
+                  height: 480,
+                  decoration: BoxDecoration(border: Border.all()),
+                  child: Center(
+                    child: videoPanel(
+                      agoraEngine,
+                      widget.channelName,
+                      uid,
+                      _remoteUid,
+                      _isHost,
+                    ),
+                  ),
+                )
+              else
+                Container(
+                    height: 480,
+                    decoration: BoxDecoration(border: Border.all()),
+                    child: const Center(
+                      child: Text('Press play button'),
+                    )
+                ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: ElevatedButton(
+                      child: const Text("Play"),
+                      onPressed: () => {join()},
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  Expanded(
+                    child: ElevatedButton(
+                      child: const Text("Stop"),
+                      onPressed: () => {leave()},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          )
+      ),
+    );
   }
 
 }
