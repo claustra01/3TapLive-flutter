02980d81cafe0656602e9115fd4eaf1fa06dda2c
claustra01 <shinya430.g@gmail.com>
2023-09-20 16:23:00 +0900

    :sparkles: send comment

--- lib/component/comment_form.dart
+++ lib/component/comment_form.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:hackz_tyranno/infrastructure/graphql.dart';
+import 'package:hackz_tyranno/component/dialog.dart';
 
 class CommentForm extends ConsumerStatefulWidget {
   final String channelName;
@@ -11,6 +15,34 @@ class CommentForm extends ConsumerStatefulWidget {
 
 class CommentFormState extends ConsumerState<CommentForm> {
   final commentController = TextEditingController();
+
+  void _sendComment() async {
+
+    // input validation
+    if (commentController.text == '') {
+      if (!mounted) return;
+      showAlertDialog(context, "Error", "Enter a Comment");
+      return;
+    }
+
+    // build graphql query
+    User? user = FirebaseAuth.instance.currentUser;
+    final String query = """
+      mutation {
+        createComment(body: "${commentController.text}", channel: "${widget.channelName}", owner: "${user?.displayName}") {
+          body
+        }
+      }
+    """;
+
+    // fetch graphql api
+    final response = await fetchGraphql(query);
+    if (response == null) {
+      if (!mounted) return;
+      showAlertDialog(context, "Error", "Server error");
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,7 +64,7 @@ class CommentFormState extends ConsumerState<CommentForm> {
         Container(
           margin: const EdgeInsets.only(right: 10),
           child: ElevatedButton(
-            onPressed: () {},
+            onPressed: _sendComment,
             child: const Icon(Icons.send),
           ),
         ),
