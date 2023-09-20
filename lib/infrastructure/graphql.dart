cad58927b112e540a753f989ca8637b180db3e6c
claustra01 <shinya430.g@gmail.com>
2023-09-20 14:23:41 +0900

    :construction: get comment by subscription

--- lib/infrastructure/graphql.dart
+++ lib/infrastructure/graphql.dart
@@ -1,12 +1,14 @@
 import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:web_socket_channel/io.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 dynamic fetchGraphql(String query) async {
 
   // graphql client
-  GraphQLClient getRickAndMortyApiClient() {
+  GraphQLClient getGqlClient() {
     final Link link = HttpLink(
-      dotenv.get('APP_SERVER_URL'),
+      dotenv.get('GRAPHQL_SERVER_URL'),
     );
     return GraphQLClient(
       cache: GraphQLCache(),
@@ -16,7 +18,7 @@ dynamic fetchGraphql(String query) async {
 
   // get response
   try {
-    final GraphQLClient client = getRickAndMortyApiClient();
+    final GraphQLClient client = getGqlClient();
     final QueryOptions options = QueryOptions(document: gql(query));
     final QueryResult response = await client.query(options);
     return response;
@@ -24,4 +26,50 @@ dynamic fetchGraphql(String query) async {
     return null;
   }
 
+}
+
+WebSocketChannel? connectGqlSubscription(String query, String connId) {
+  try {
+    // websocket graphql client
+    final String url = dotenv.get('WEBSOCKET_SERVER_URL');
+    final channel = IOWebSocketChannel.connect(
+        Uri.parse(url),
+        headers: {
+          'Sec-WebSocket-Protocol': 'graphql-ws',
+        }
+    );
+
+    // init graphql connection
+    final String initMessage = """
+      {
+        "id": "$connId",
+        "type": "connection_init"
+      }
+    """;
+    channel.sink.add(initMessage);
+
+    // set subscription query
+    String startMessage = """
+      {
+        "id": "$connId",
+        "type": "start",
+        "payload": {
+          "variables": {},
+          "extensions": {},
+          "operationName": null,
+          "query": "${query.replaceAll('\n', '')}"
+        }
+      }
+    """;
+    print(startMessage);
+    channel.sink.add(startMessage);
+    return channel;
+
+  } catch (e) {
+    return null;
+  }
+}
+
+void closeGqlSubscription(WebSocketChannel channel) {
+  channel.sink.close();
 }
